6692a3aff018164ee045c83123ffb937f310898e
yunlei0930 <46972608+yunlei0930@users.noreply.github.com>
2019-04-29 18:01:42 -0500

    Realized the click and highlight function

--- js/main.js
+++ js/main.js
@@ -22,8 +22,153 @@ function createMap(){
 
 	
 	//create the map title
-	createTitle(map);   
+	createTitle(map); 
 
+
+	
+	d3.csv("/data/travel_distance_WI_his.csv").then(callback);
+	
+	expressed = "distance_from_home";
+	function callback(data){
+		var colorScale = makeColorScale(data);
+
+		setChart(data,colorScale);
+	};
+	
+};
+
+//function to create color scale generator
+function makeColorScale(data){
+    var colorClasses = [
+		'#FFEDA0',
+		'#FED976',
+		'#FEB24C',
+		"#FD8D3C",
+		"#FC4E2A",
+		"#E31A1C",
+		"#BD0026",
+        "#800026"			
+    ];
+
+    //create color scale generator
+    var colorScale = d3.scaleQuantile()
+        .range(colorClasses);
+
+    //build array of all values of the expressed attribute
+    var domainArray = [];
+    for (var i=0; i<data.length; i++){
+        var val = parseFloat(data[i]["Count_of_distance_from_home"]);
+        domainArray.push(val);
+    };
+
+    //assign array of expressed values as scale domain
+    colorScale.domain(domainArray);
+	interval = colorScale.quantiles();
+	
+    return colorScale;
+};
+
+//function to create coordinated bar chart
+function setChart(csvData,colorScale){
+
+	var w = window.innerWidth * 0.8, h = window.innerHeight * 0.25,
+		translate = "translate(" + 1 + "," + 1 + ")";
+    //Example 1.5 line 1...container block
+    var chart = d3.select("body") //get the <body> element from the DOM
+        .append("svg") //put a new svg in the body
+        .attr("width", w) //assign the width
+        .attr("height", h) //assign the height
+        .attr("class", "chart"); //assign a class name
+	
+   var chartBackground = chart.append("rect")
+        .attr("class", "chartBackground")
+        .attr("width", w*0.95)
+        .attr("height", h); //svg background color	
+
+	console.log(csvData);
+    var bars = chart.selectAll(".bar")
+        .data(csvData)
+        .enter()
+        .append("rect")
+        .attr("class", function(d){
+            return "bar " + d.RowLabels;
+        })
+        .attr("width",w/csvData.length-1);
+ 
+	var yScale = d3.scaleLinear()
+	.range([150,0])
+	.domain([0, 1645]);       
+ 
+ //create vertical axis generator
+    var yAxis = d3.axisLeft()
+        .scale(yScale);
+
+    //place axis
+    var axis = chart.append("g")
+        .attr("class", "axis")
+		.attr("transform", translate)
+        .call(yAxis);
+
+    //create frame for chart border
+    var chartFrame = chart.append("rect")
+        .attr("class", "chartFrame")
+        .attr("width", w*0.95)
+        .attr("height", h)
+		.attr("transform", translate);;	
+		
+	//set bar positions, heights and colors
+	updateChart(bars, csvData.length, colorScale);
+
+};
+
+//function to position, size, and color bars in chart
+function updateChart(bars, n, colorScale){
+	//chart frame dimensions
+	var chartWidth = window.innerWidth * 0.75,
+	chartHeight = window.innerHeight * 0.25,
+	leftPadding = 1,
+	rightPadding = 1,
+	topBottomPadding = 5,
+	chartInnerWidth = chartWidth - leftPadding - rightPadding,
+	chartInnerHeight = chartHeight - topBottomPadding,
+	translate = "translate(" + leftPadding + "," + topBottomPadding + ")";
+	
+
+	console.log(chartHeight);
+	//create a scale to size bars proportionally to frame
+	var yScale = d3.scaleLinear()
+	.range([150,0])
+	.domain([0, 1645]);
+	
+	expressed="Count_of_distance_from_home";
+
+    //position bars
+    bars.attr("x", function(d, i){
+            return i * (chartWidth/ n) + leftPadding;
+        })
+        //size/resize bars
+        .attr("height", function(d, i){
+            return chartHeight - yScale(parseFloat(d[expressed]));
+        })
+        .attr("y", function(d, i){
+            return yScale(parseFloat(d[expressed])) + topBottomPadding;
+        })
+		.style("fill", function(d){
+            return choropleth(d, colorScale);
+        });;
+
+};
+
+//function to test for data value and return color
+function choropleth(props, colorScale){
+    //make sure attribute value is a number
+    var val = parseFloat(props["Count_of_distance_from_home"]);
+    //if attribute value exists, assign a color; otherwise assign gray
+    if (typeof val == 'number' && !isNaN(val)){
+        return colorScale(val);
+    } else {
+        return "#CCC";
+    };
 };
 
 //add the title to the map
@@ -103,7 +248,7 @@ function onEachFeature1(feature,attributes,map,layer){
 			this.closePopup();
 		},
 		click: function(){
-			highlightCBG(this,map);
+
 			map.eachLayer(function (layer) {
 				map.removeLayer(layer);
 			});
@@ -113,7 +258,23 @@ function onEachFeature1(feature,attributes,map,layer){
 				attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>' +'<br>Data sources: United States Census Bureau <br> Creator: Yunlei Liang'
 			}).addTo(map);
 			
-			AddGrayFeatures(map);			
+			//AddGrayFeatures(map);	
+			highlightCBG(this,map);
+			
+			var cbg_d = this.feature.properties.WI_cbgs_Ce;
+			
+			d3.csv("/data/home_mobility_WI.csv").then(callback);
+	
+			function callback(data){
+				var HighlightData = [];
+				for (i =0; i<data.length; i++){
+					if (cbg_d == data[i]["cbg_d"]){
+						HighlightData.push(data[i]["cbg_o"]);
+					}
+				}
+				console.log(HighlightData);
+				HighlightFeatures(HighlightData,map);
+			};
 		}
 			
     });
@@ -122,17 +283,94 @@ function onEachFeature1(feature,attributes,map,layer){
 
 function highlightCBG(e,map){
 	
-	console.log(e);
-	e.addTo(map);
 	//var layer = e.target;
+	e.addTo(map);
 	
+};
+
+
+function HighlightFeatures(data,map){
+	$.ajax("data/WI_cbg_TravelDis.geojson", {
+        dataType: "json",
+        success: function(response){
+            //create an attributes array
+            var attributes = processData(response,"travel_d_4");
+			//get the generated map layer
+			AddHighlight(response,map,attributes,data);
+        }
+    });
+
+};
+
+
+//create the choropleth map
+function AddHighlight(response, map, attributes,data){
+	
+    //create a Leaflet GeoJSON layer
+    L.geoJson(response, {
+		style: function (feature) {		
+			return styleHightlighted(feature,attributes,data);			
+		}
+    }).addTo(map);
+    
+
+};
+
+
+function styleHightlighted(feature,attributes,data){
+
+	var attribute = attributes[1];
+//	var checkExistence = checkExistence(feature.properties.travel_d_4,data);
+	for (i=0; i<data.length; i++){
+		if (feature.properties.travel_d_4 == data[i]){
+			return {
+			fillColor: "#87CEEB",
+			weight: 0.4,
+			opacity: 1,
+			color: 'white',
+			dashArray: '3',
+			fillOpacity: 10
+		} 
+		};
+		
+	}
+	
+	return {
+			fillColor: "#CCC",
+			weight: 0.4,
+			opacity: 1,
+			color: 'white',
+			dashArray: '3',
+			fillOpacity: 10
+		} 
+
+
+	
+
+};
+
+
+
+
+//generate the choropleth map data and the two layer control
+function getDataChoro(map){
+    //load the data
+    $.ajax("data/WI_cbg_TravelDis.geojson", {
+        dataType: "json",
+        success: function(response){
+            //create an attributes array
+            var attributes = processData(response,"travel_d_1");
+			//get the generated map layer
+			createChoropleth(response, map, attributes);
+        }
+    });
 	
 };
 
 
 //create the choropleth map
 function createChoropleth(response, map, attributes){
-	
+
     //create a Leaflet GeoJSON layer
     var geojson1 = L.geoJson(response, {
 		onEachFeature: function(feature,layer){
@@ -176,7 +414,7 @@ function createChoropleth(response, map, attributes){
 
 
 //build an attributes array from the data
-function processData(data){
+function processData(data,field){
     //empty array to hold attributes
     var attributes = [];
 
@@ -186,7 +424,7 @@ function processData(data){
     //push each attribute name into attributes array
     for (var attribute in properties){
         //only take attributes with population values
-        if (attribute.indexOf("travel_d_1") > -1){
+        if (attribute.indexOf(field) > -1){
             attributes.push(attribute);
         };
     };
@@ -202,7 +440,7 @@ function getDataChoro(map){
         dataType: "json",
         success: function(response){
             //create an attributes array
-            var attributes = processData(response);
+            var attributes = processData(response,"travel_d_1");
 			//get the generated map layer
 			createChoropleth(response, map, attributes);
         }
@@ -224,8 +462,7 @@ function AddGrayFeatures(map){
 			geojson1.addTo(map);
         }
     });
-		
-	
+			
 };
 
 function style1(feature){
